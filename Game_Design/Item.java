527e0b4a40577a530e742caffecbb5d042dfda65
joshuaven <youzeiteikoku@gmail.com>
2018-09-28 04:43:32 +0800

    rename the Items class to Item and modified the Room class

---
+++ Game_Design/Item.java
@@ -0,0 +1,9 @@
+class Item {
+    String name;
+    String description;
+
+    protected Item(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+}
--- Game_Design/Room.java
+++ Game_Design/Room.java
@@ -1,13 +1,49 @@
-import Items.java;
+import Item.java;
 
 class Room {
     int roomNumber;
-    int numOfItemsForRoom;
-    Items[] items;
+    private Item[] items;
     
-    protected Room(int roomNumber) {
+    private Room(int roomNumber, int numOfItemsForRoom) {
         this.roomNumber = roomNumber;
-        this.items = new Items[numOfItemsForRoom];
+        this.items = new Item[numOfItemsForRoom];
+        Item dummyItem = extracted();
+        for (int i = 0; i < items.length; i++) {
+            items[i] = dummyItem;
+        }
     }
 
+    private Item extracted() {
+        Item dummy= new Item("", "");
+        return dummy;
+    }
+
+    private void addItem(String name, String desc) {
+        Item newItem = new Item(name, desc);
+		for (int i = 0; i < items.length; i++) {
+            if (items[i].name.equals(extracted().name) && items[i].description.equals(extracted().description)) {
+                items[i] = newItem;
+                return;
+            }
+        }
+    }
+
+    public void viewItems() {
+        for (int i = 0; i < items.length; i++) {
+            if (!items[i].name.equals(extracted().name) && !items[i].description.equals(extracted().description)) {
+                System.out.println(items[i].name + ": " + items[i].description);
+            }
+        }
+    }
+
+     // test in the class
+     private void test() {
+        addItem("LADDLE", "Rusted but still in good quality!");
+        viewItems();
+    }
+
+    public static void main(String[] args) {
+        Room sample = new Room(0, 3);
+        sample.test();
+    }
 }
