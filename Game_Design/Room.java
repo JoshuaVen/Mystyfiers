26f560332b4e4587771a82c518a904c247f8a1e9
NAP <31622277+NishaAlexis@users.noreply.github.com>
2018-10-09 23:03:35 +0800

    Added choices()

--- Game_Design/Room.java
+++ Game_Design/Room.java
@@ -26,10 +26,41 @@ class Room {
         }
     }
 
+	public void choices() {
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter command: ");
+		String command;
+		command = in.nextLine();
+		switch (command.toLowerCase()) {
+		case "look":
+			System.out.println("The room consists of a working table, book shelf, etc...");
+			break;
+		case "examine":
+			System.out.println("There's a note on the table saying 'You must escape the room or else...'");
+			break;
+		case "inventory":
+			System.out.println("You have: ");
+			viewItems();
+			break;
+		case "get":
+			System.out.println("You have gotten the object");
+			break;
+		case "read":
+			System.out.println("You are reading...");
+			break;
+		case "":
+			System.out.println("I beg your pardon?");
+			break;
+		default:
+			System.out.println("That's not a verb I recognize. (Enter again.)");
+			break;		
+		}	
+	}
+	
     public void viewItems() {
         for (int i = 0; i < items.length; i++) {
             if (!items[i].name.equals(extracted().name) && !items[i].description.equals(extracted().description)) {
-                System.out.println(items[i].name + ": " + items[i].description);
+                System.out.println(items[i].name);
             }
         }
     }
@@ -45,5 +76,6 @@ class Room {
     public static void main(String[] args) {
         Room sample = new Room(0, 3);
         sample.test();
+        sample.choices();
     }
 }
