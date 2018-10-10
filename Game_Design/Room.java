df6212e3b302113f6a479b2045eb860eb4dc3b78
NishaPadura <alexispadura@gmail.com>
2018-10-10 15:21:37 +0800

    Modified choices(), Room(), and viewItems()

--- Game_Design/Room.java
+++ Game_Design/Room.java
@@ -1,36 +1,38 @@
-class Room {
-    int roomNumber;
-    private Item[] items;
-    
-    private Room(int roomNumber, int numOfItemsForRoom) {
-        this.roomNumber = roomNumber;
-        this.items = new Item[numOfItemsForRoom];
-        Item dummyItem = extracted();
-        for (int i = 0; i < items.length; i++) {
-            items[i] = dummyItem;
-        }
-    }
+import java.util.Scanner;
 
-    private Item extracted() {
-        Item dummy= new Item("", "");
-        return dummy;
-    }
+public class Room {
+	int roomNumber;
+	private Item[] items;
+	    
+	private Room(int roomNumber) {
+		this.roomNumber = roomNumber;
+		int MAXNumofItems = 5;
+		this.items = new Item[MAXNumofItems];
+	    Item dummyItem = extracted();
+	    for (int i = 0; i < items.length; i++) {
+	    	items[i] = dummyItem;
+	    }
+	}
 
-    private void addItem(String name, String desc) throws ArrayIndexOutOfBoundsException {
-        Item newItem = new Item(name, desc);
-		for (int i = 0; i < items.length; i++) {
-            if (items[i].name.equals(extracted().name) && items[i].description.equals(extracted().description)) {
-                items[i] = newItem;
-                return;
-            }
-        }
-    }
+	private Item extracted() {
+	    Item dummy= new Item("", "");
+	    return dummy;
+	}
 
+	private void addItem(String name, String desc) throws ArrayIndexOutOfBoundsException {
+	    Item newItem = new Item(name, desc);
+	    for (int i = 0; i < items.length; i++) {
+	        if (items[i].name.equals(extracted().name) && items[i].description.equals(extracted().description)) {
+	            items[i] = newItem;
+	            return;
+	        }
+	    }
+	}
+	
 	public void choices() {
 		Scanner in = new Scanner(System.in);
-		System.out.println("Enter command: ");
-		String command;
-		command = in.nextLine();
+		System.out.print("> ");
+		String command = in.nextLine();
 		switch (command.toLowerCase()) {
 		case "look":
 			System.out.println("The room consists of a working table, book shelf, etc...");
@@ -41,6 +43,7 @@ class Room {
 		case "inventory":
 			System.out.println("You have: ");
 			viewItems();
+			choices();
 			break;
 		case "get":
 			System.out.println("You have gotten the object");
@@ -48,34 +51,47 @@ class Room {
 		case "read":
 			System.out.println("You are reading...");
 			break;
+		case "help":
+			System.out.println("Help Basic Commands are look, examine, inventory, get, read...");
+			choices();
+			break;
 		case "":
 			System.out.println("I beg your pardon?");
+			choices();
 			break;
 		default:
-			System.out.println("That's not a verb I recognize. (Enter again.)");
+			System.out.println("That's not a verb I recognize.");
+			choices();
 			break;		
-		}	
+		}
+		in.close();
+	}
+
+	public void viewItems() {
+	    for (int i = 0; i < items.length; i++) {
+	        if (!items[i].name.equals(extracted().name) && !items[i].description.equals(extracted().description)) {
+	            System.out.println("\t" + items[i].name);
+	        }
+	    }
+	}
+
+	// test in the class
+	private void test() {
+	    addItem("LADDLE", "Rusted but still in good quality!");
+	    addItem("Key", "Small key that opens a cabinet in the Sala");
+	    addItem("Plastic Bottle", "Half full and contains dirty water");
+	    addItem("Book", "Hard-bound Harry Potter book 1");
+	    addItem("Paper", "Note");
+	    addItem("Small key", "For a padlock");
+	  //viewItems();
+	 // choices();
 	}
 	
-    public void viewItems() {
-        for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals(extracted().name) && !items[i].description.equals(extracted().description)) {
-                System.out.println(items[i].name);
-            }
-        }
-    }
-
-     // test in the class
-     private void test() {
-        addItem("LADDLE", "Rusted but still in good quality!");
-        addItem("Key", "Small key that opens a cabinet in the Sala");
-        addItem("Plastic Bottle", "Half full and contains dirty water");
-        viewItems();
-    }
-
-    public static void main(String[] args) {
-        Room sample = new Room(0, 3);
-        sample.test();
-        sample.choices();
-    }
+	public static void main(String[] args) {
+	    Room start = new Room(0);
+	    start.test();
+	    System.out.println("Introduction: You have been kidnapped...");
+	    System.out.println();
+		start.choices();
+	}
 }
